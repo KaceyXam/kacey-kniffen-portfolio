6c84057ea4816ba0ba1e813baf4471a5d7548bb3
kaceykniffen <kaceykniffen@gmail.com>
2023-03-07 23:01:20 -0600

    Add Storyblok for CMS

--- v2-0/astro.config.mjs
+++ v2-0/astro.config.mjs
@@ -1,9 +1,25 @@
-import { defineConfig } from 'astro/config';
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import { defineConfig } from "astro/config";
+
+import storyblok from "@storyblok/astro";
 
 // https://astro.build/config
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+	integrations: [
+		solidJs(),
+		storyblok({
+			accessToken: process.env.STORYBLOK_TOKEN,
+			apiOptions: {
+				region: "us",
+			},
+			components: {
+				blogpost: "storyblok/BlogPost",
+			},
+		}),
+	],
 });
