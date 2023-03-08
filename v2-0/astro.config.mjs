68363b051c57793c09f7f9a3c5f542ec9dffadf8
kaceykniffen <kaceykniffen@gmail.com>
2023-03-07 23:06:13 -0600

    Remove Storyblok
    I don't need a cms like storyblok for this project. I decided that I can easily get away with writing my blog posts in Markdown and storing them on github

--- v2-0/astro.config.mjs
+++ v2-0/astro.config.mjs
@@ -3,23 +3,10 @@ dotenv.config();
 
 import { defineConfig } from "astro/config";
 
-import storyblok from "@storyblok/astro";
-
 // https://astro.build/config
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		solidJs(),
-		storyblok({
-			accessToken: process.env.STORYBLOK_TOKEN,
-			apiOptions: {
-				region: "us",
-			},
-			components: {
-				blogpost: "storyblok/BlogPost",
-			},
-		}),
-	],
+	integrations: [solidJs()],
 });
