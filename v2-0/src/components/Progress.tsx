0139d283b227a88f78207c3d3e9be357ab4e9951
kaceykniffen <kaceykniffen@gmail.com>
2023-03-11 01:48:09 -0600

    Begin work on Scroll progress bar

---
+++ v2-0/src/components/Progress.tsx
@@ -0,0 +1,24 @@
+import { createEffect, createMemo, createSignal } from "solid-js";
+
+import "./Progress.scss";
+
+export function Progress() {
+	const [scrollPos, setScrollPos] = createSignal(0);
+
+	createEffect(() => {
+		window.addEventListener("scroll", () => {
+			const { scrollHeight, clientHeight, scrollTop } =
+				document.documentElement;
+			const winScroll = document.body.scrollTop || scrollTop;
+			const winHeight = scrollHeight - clientHeight;
+
+			const scrolled = (winScroll / winHeight) * 100;
+			if (winHeight > 0) {
+				setScrollPos(scrolled);
+			} else {
+				setScrollPos(0);
+			}
+		});
+	});
+	return <div class="progress-bar">{scrollPos()}</div>;
+}
